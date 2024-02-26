63f936146b49826daa8f7a0e8a7ceb1cef1160dd
DanaPar <danaprn@gmail.com>
2024-02-26 18:17:28 +0200

    added removing from array functions

--- PHP/Arrays/buildArray.php
+++ PHP/Arrays/buildArray.php
@@ -2,6 +2,9 @@
 
 class MyArray {
 
+    private int $length;
+    private $data;
+
     function __construct()
     {
         $this->length = 0;
@@ -13,16 +16,44 @@ class MyArray {
         return $this->data[$index];
     }
 
-    function add($item)
+    function add($item): int //same as array_push()
     {
        $this->data[$this->length] = $item;
        $this->length++;
        return $this->length;
     }
 
+    function removeLast() //sames as array_pop()
+    {
+        $lastItem = $this->data[$this->length -1];
+        unset($this->data[$this->length -1]);
+        $this->length--;
+        return $lastItem;
+    }
+
+    function delete($index)
+    {
+        $item = $this->data[$index];
+        $this->shiftItems($index);
+        return $item;
+    }
+
+    function shiftItems($index)
+    {
+        for ($i = $index; $i < $this->length - 1; $i++) {
+            $this->data[$i] = $this->data[$i + 1];
+        }
+        unset($this->data[$this->length - 1]);
+        $this->length--;
+    }
 
 }
 
 $array  = new MyArray();
-$array->add("NIce day");
+$array->add("Nice");
+$array->add("day");
+$array->add("!");
+$array->delete(1);
 print_r($array);
+
+
